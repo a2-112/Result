ce631b144189b3d9fb7a7733dde8293dabe65b94
Eziefule Judith <juditheziefule1@gmail.com>
2025-02-20 11:51:43 -0800

    second commit

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-// fetch('data.json')
-// // fetch the JSON file
-// .then(response => response.json())
-// // convert the JSON file to a Javascript object
-// .then(data => {
-//       // this uses the var name data to fetch the data.json file into the html element
-//     const resultsSummary = document.querySelectorAll('.summary > div');
-//      // Select all predefined summary items
-//     data.forEach((result, index) => {
-//         if (resultsSummary[index]) {// Ensure it doesn't go out of bounds
-//             const resultImage = resultsSummary[index].querySelector('img');
-             
-//         const resultName = resultsSummary[index].querySelector('h3');
-       
-//         const resultDescription = resultsSummary[index].querySelector('p');
-
-//             resultImage.src = result.icon;
-//             resultImage.alt = result.category;
-//              resultName.textContent = `${result.category}`;
-//                     resultDescription.textContent =  `Score: ${result.score}`;
-//         }
-       
-//     });
-// })
-// .catch(error => {
-//     console.error("Error fetching JSON", error);
-// });
-// // Error handling for JSON fetch
 
 fetch('data.json')
   .then(response => response.json()) 
